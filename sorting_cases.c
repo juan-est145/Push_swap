3dde8b6626e9a7e49e13cc2ee17cf4843b07f8d4
Juan Jose Estrella Ramirez <128648875+juan-est145@users.noreply.github.com>
2024-02-05 15:53:57 +0100

    Three of the base cases for a stack of three work

--- sorting_cases.c
+++ sorting_cases.c
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 15:56:13 by juan              #+#    #+#             */
-/*   Updated: 2024/02/05 15:41:11 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/02/05 15:53:12 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,12 @@ void	ft_sort_three(t_stack_node **a)
 		&& (*a)->value > (*a)->next->next->value
 		&& (*a)->next->value < (*a)->next->next->value)
 		rotate_a(a);
+	else if ((*a)->next->value > (*a)->value
+		&& (*a)->next->value > (*a)->next->next->value)
+	{
+		swap_a(a);
+		rotate_a(a);
+	}
 }
+
+//Case 1, 3 and 4 done
